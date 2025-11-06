c5c8f43bfb01f2d672ae0286710923981744e656
Akash Mishra <114408445+Akash324m@users.noreply.github.com>
2025-11-06 16:12:52 +0530

    Time: 2 ms (63.86%) | Memory: 13.4 MB (62.55%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -4,7 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums = set(nums)
-        for x in range(len(nums)+1):
-            if x not in nums:
-                return x
+        # nums = set(nums)
+        # for x in range(len(nums)+1):
+        #     if x not in nums:
+        #         return x
+        s= 0
+        ts= 0
+        for x in nums:
+            s+=x
+        for y in range(len(nums)+1):
+            ts+=y
+        return ts-s
