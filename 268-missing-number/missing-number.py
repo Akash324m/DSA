89f7cf8c4ccc546092f1ea90c255ebee95c13dba
Akash Mishra <114408445+Akash324m@users.noreply.github.com>
2025-11-06 16:17:41 +0530

    Time: 3 ms (61.63%) | Memory: 13.2 MB (99.03%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -8,10 +8,11 @@ class Solution(object):
         # for x in range(len(nums)+1):
         #     if x not in nums:
         #         return x
-        s= 0
-        ts= 0
-        for x in nums:
-            s+=x
-        for y in range(len(nums)+1):
-            ts+=y
-        return ts-s
+        # s= 0
+        # ts= 0
+        # for x in nums:
+        #     s+=x
+        # for y in range(len(nums)+1):
+        #     ts+=y
+        # return ts-s
+        return sum(range(0,len(nums)+1)) - sum(nums)
